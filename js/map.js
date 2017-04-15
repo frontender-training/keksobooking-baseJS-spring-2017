cba9b2843a2294dee13ee2d129b631faa56f1589
Yulia Maximova <juliam2007@mail.ru>
2017-04-15 17:30:28 +0300

    Правки кода

--- js/map.js
+++ js/map.js
@@ -1,45 +1,24 @@
 'use strict';
 
-function createAds(i) {
-  
-  var AVATARS = ['img/avatars/user01.png',
-               'img/avatars/user02.png',
-               'img/avatars/user03.png',
-               'img/avatars/user04.png',
-               'img/avatars/user05.png',
-               'img/avatars/user06.png',
-               'img/avatars/user07.png',
-               'img/avatars/user08.png'
-             ];
-  var TITLE_ADS = ['Большая уютная квартира',
-             'Маленькая неуютная квартира',
-             'Огромный прекрасный дворец',
-             'Маленький ужасный дворец',
-             'Красивый гостевой домик',
-             'Некрасивый негостеприимный домик',
-             'Уютное бунгало далеко от моря',
-             'Неуютное бунгало по колено в воде'
-             ];
-
+function createAds(i) {  
+  var AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+  var TITLE_ADS = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
   var TYPE_OF_ROOMS = ['flat', 'house', 'bungalo'];
   var NUMBER_OF_ROOMS = ['1', '2', '3', '4', '5'];
   var TIME_CHECKIN = ['12:00', '13:00', '14:00'];
   var TIME_CHECKOUT = ['12:00', '13:00', '14:00'];
   var FACILITY = ['wifi', 'dishwasher', 'parking', 'elevator', 'conditioner'];
 
-
   // Функция, меняющая порядок в массиве
   function shuffle (array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        // Задаем переменную, у которой числовое значение будет определяться рандомно
-        var randomIndex = Math.floor(Math.random() * (i + 1));
-        // Задаем переменную, у которой числовое значение будет определяться рандомно
-        var temporaryValue = array[i];
-        array[i] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    for (var i = array.length - 1; i > 0; i--) { // Задаем переменную, у которой числовое значение будет определяться рандомно
+    var randomIndex = Math.floor(Math.random() * (i + 1)); // Задаем переменную, у которой числовое значение будет определяться рандомно
+    var temporaryValue = array[i];
+    array[i] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
     }    
     return array;
-  }
+    }
   
   function getRandom (min, max) {
     return min + Math.floor(Math.random() * (max + 1 - min));
