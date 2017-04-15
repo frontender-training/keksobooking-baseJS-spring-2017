2bb4793073a65a367b151111291d17b8b0728ef8
Yulia Maximova <juliam2007@mail.ru>
2017-04-15 15:18:55 +0300

    Исправление ошибок по тревису

--- js/map.js
+++ js/map.js
@@ -2,15 +2,7 @@
 
 function createAds (i) {
   
-  var AVATARS = ['img/avatars/user01.png',
-               'img/avatars/user02.png',
-               'img/avatars/user03.png',
-               'img/avatars/user04.png',
-               'img/avatars/user05.png',
-               'img/avatars/user06.png',
-               'img/avatars/user07.png',
-               'img/avatars/user08.png'
-             ];
+  var AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
   var TITLE_ADS = ['Большая уютная квартира',
              'Маленькая неуютная квартира',
              'Огромный прекрасный дворец',
